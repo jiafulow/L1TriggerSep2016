04d6eb0bc28ef10567bad514fb51fdefb1cf03dc
Jia Fu Low <jia.fu.low@cern.ch>
2016-10-04 20:35:48 -0400

    Fix (unconfirmed) bug in EMTFSectorProcessorLUT

--- L1TMuonEndCap/src/EMTFSectorProcessorLUT.cc
+++ L1TMuonEndCap/src/EMTFSectorProcessorLUT.cc
@@ -105,8 +105,8 @@ void EMTFSectorProcessorLUT::read(const std::string& ph_th_lut) {
   // start phi of each chamber in reduced precision, for zone hits,
   // with negative offset to allow for possible chamber movement
   ph_init_hard_ = {
-    39,  57,  76, 39,  58,  76, 41,  60,  79, 39,  57,  76, 21, 21, 23, 21,
-    95, 114, 132, 95, 114, 133, 98, 116, 135, 95, 114, 132,  0,  0,  0,  0,
+    39,  57,  76, 39,  58,  76, 41,  60,  79, 39,  57,  76,  0,  0,  0,  0,
+    95, 114, 132, 95, 114, 133, 98, 116, 135, 95, 114, 132, 21, 21, 23, 21,
     38,  76, 113, 39,  58,  76, 95, 114, 132,  1,  21,   0,  0,  0,  0,  0,
     38,  76, 113, 39,  58,  76, 95, 114, 132,  1,  21,   0,  0,  0,  0,  0,
     38,  76, 113, 38,  57,  76, 95, 113, 132,  1,  20,   0,  0,  0,  0,  0
