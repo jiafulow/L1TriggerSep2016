8e8ec6796b12b066ae943da1f93b9546d58cb8f9
Jia Fu Low <jia.fu.low@cern.ch>
2017-03-16 16:18:36 -0400

    Remove python/simEmtfDigis_cfi.py

--- L1TMuonEndCap/python/simEmtfDigis_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from L1Trigger.L1TMuonEndCap.simEmtfDigis_cfi import simEmtfDigisMC, simEmtfDigisData, simEmtfDigis
-
-simEmtfDigisMC = simEmtfDigisMC.clone()
-simEmtfDigisData = simEmtfDigisData.clone()
-simEmtfDigis = simEmtfDigis.clone()
-
