5e158d343b277a641a8fd7bb6c3a76919a4daa08
Jia Fu Low <jia.fu.low@cern.ch>
2018-02-20 19:43:26 -0500

    Whitespace fix

--- L1TMuonEndCap/interface/Common.h
+++ L1TMuonEndCap/interface/Common.h
@@ -43,24 +43,24 @@ namespace emtf {
 
   // Phase 2 Geometry a.k.a. HL-LHC
   constexpr int PHASE_TWO_GEOMETRY = 0;
-  
+
   // from DataFormats/MuonDetId/interface/CSCDetId.h
   constexpr int MIN_ENDCAP = 1;
   constexpr int MAX_ENDCAP = 2;
-  
+
   // from DataFormats/MuonDetId/interface/CSCTriggerNumbering.h
   constexpr int MIN_TRIGSECTOR = 1;
   constexpr int MAX_TRIGSECTOR = 6;
   constexpr int NUM_SECTORS = 12;
-  
+
   // Zones
   constexpr int NUM_ZONES = 4;
   constexpr int NUM_ZONE_HITS = 160;
-  
+
   // Stations
   constexpr int NUM_STATIONS = 4;
   constexpr int NUM_STATION_PAIRS = 6;
-  
+
   // Fixed-size arrays
   template<typename T>
   using sector_array = std::array<T, NUM_SECTORS>;
